0d80a22b2744f304e41fe6e76283ee2136660b28
Su Bae <tugisuwon@gmail.com>
2025-10-16 19:49:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import streamlit as st
 import json
 from utils import extract_text_from_pdf, query_mistral, build_prompt
 
+st.write(st.secrets['hugging-face'])
+HF_TOKEN = st.secrets['hugging-face']['api-token']
+st.write(HF_TOKEN)
 st.set_page_config(page_title="Purchase Order Extractor", layout="wide")
 st.title("📄 Purchase Order Extractor")
 
