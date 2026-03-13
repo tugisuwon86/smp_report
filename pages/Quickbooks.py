f3633a7b621c32b0e0254af1715da80b336cdc4a
Su Bae <tugisuwon@gmail.com>
2026-03-13 14:00:06 -0400

    Update Quickbooks.py

--- pages/Quickbooks.py
+++ pages/Quickbooks.py
@@ -154,6 +154,7 @@ if not df.empty:
     qb_items, qb_vendors, qb_customers = load_qb_items()
 
     matched_rows = df.to_json(orient="records")
+    st.write(matched_rows[0])
     
     missing_items = validate_items_against_qb(matched_rows, qb_items)
 
