c67c29547b2eaa4e2c9d94986fc54008e6b834ab
Su Bae <tugisuwon@gmail.com>
2026-03-20 14:19:21 -0400

    Update Quickbooks.py

--- pages/Quickbooks.py
+++ pages/Quickbooks.py
@@ -301,8 +301,23 @@ if not df.empty:
     output = []
     for _, row in df.iterrows():
         meta_match, factor = best_meta_match(r, meta_df, option_company)
-        product, vlt, width, length, date, quantity, price, amount = row["product"], row["vlt"], row["width"], row["length"], row["date"], row["quantity"], row["price"], row["amount"]
-        code = 
+        # product, vlt, width, length, date, quantity, price, amount = row["product"], row["vlt"], row["width"], row["length"], row["date"], row["quantity"], row["price"], row["amount"]
+        if meta_match is not None:
+            type_code = meta_match["Type (Code)"]
+            techpia_code = meta_match["Techpia (Code)"]
+            description = meta_match["Description"]
+            pi_unit_price = float(meta_match["Price"])
+            po_unit_price = float(meta_match["PO Price"])
+            slitting = meta_match["Width Slitting"]
+            length = meta_match["Length"]
+        matched_rows.append({
+            "product": type_code,
+            "description": row["description"],
+            "vlt": row["vlt"],
+            "quantity": row["quantity"],
+            "price": row["price"],
+            "amount": row["amount"]
+        })
 
     with col1:
         download_button(
